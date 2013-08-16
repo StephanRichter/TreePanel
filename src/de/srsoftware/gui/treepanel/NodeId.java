b9f3837ca864437a35e879277bb64491152438c9
Stephan Richter <github@srsoftware.de>
2013-08-16 10:18:39 +0200

    umfangreiche umstrukturierungen vorgenommen

---
+++ src/de/srsoftware/gui/treepanel/NodeId.java
@@ -0,0 +1,14 @@
+package de.srsoftware.gui.treepanel;
+
+import java.util.Date;
+
+
+public class NodeId {
+	private String internalId=null;
+  public NodeId(){
+	  internalId=String.valueOf((new Date()).getTime());
+  }
+  public String toString(){
+	  return internalId;
+  }
+}
